e45a3642692a66436ed72f5ff7f041f65b581b68
Llimonix <58168234+Llimonix@users.noreply.github.com>
2020-02-10 10:59:45 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -17,7 +17,8 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    bot.reply_to(message, message.text)
+    if message.text == "хуй":
+         bot.reply_to(message, "ты пидор") 
 
 
 bot.polling()
